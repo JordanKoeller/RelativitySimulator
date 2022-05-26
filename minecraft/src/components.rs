f069d8caee12a0e88e3a060e8f82b5e8f6d9f466
Jordan Koeller <jkoeller12@gmail.com>
2022-05-25 20:21:52 -0500

    Ran linter

--- minecraft/src/components.rs
+++ minecraft/src/components.rs
@@ -1,9 +1,7 @@
 use specs::{Component, VecStorage};
 
-use crate::prefabs::MCBlock;
 
 pub struct ChunkComponent {
-    blocks: [MCBlock; 65536],
 }
 impl Component for ChunkComponent {
     type Storage = VecStorage<ChunkComponent>;
--- minecraft/src/prefabs/mod.rs
+++ minecraft/src/prefabs/mod.rs
@@ -1,7 +1,3 @@
-mod block;
-mod block_face;
 mod chunk;
 
-pub use self::block::*;
-pub use self::block_face::*;
 pub use self::chunk::*;
